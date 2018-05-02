54012ae4c0d0134e98337c8c759ceb686f026baf
steve <stg7@gmx.de>
2018-05-03 01:37:40 +0200

    minor reformatting

--- calculate.py
+++ calculate.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-
 import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/itu-p1203/')
 
 from p1203Pv_extended import P1203Pv_codec_extended
-
-
 from itu_p1203.__main__ import main
+
 
 if __name__ == "__main__":
     sys.exit(main({"Pv": P1203Pv_codec_extended}))
