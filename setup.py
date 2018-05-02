b3345ce0f4ee60e850e7143e2b25234b08144516
steve <stg7@gmx.de>
2018-05-03 00:58:06 +0200

    add a setup.py so that this packages can be used as external dependency

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='p1203Pv_extended',
+    version='0.1',
+    description='p1203Pv_extended',
+    author='Steve Goering',
+    author_email='stg7@gmx.de',
+    url='',
+    packages=find_packages(),
+    install_requires=[
+      'scipy',
+      'pandas'
+    ],
+    package_dir={'': 'p1203Pv_extended'},
+)
